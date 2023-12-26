1da4d9bee9ba16f9be5e79af87dd6ce481c589ba
ITS-DNY <dnyandeepgaonkar23@gmail.com>
2023-12-26 15:46:11 +0530

    third

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -288,14 +288,14 @@ function Contact() {
           </div>
           <div className="flex justify-center">
             <button type="submit">
-              <a
-                href="#_"
+              <div
+              
                 className="relative inline-flex items-center justify-center p-4 px-20 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500 "
               >
                 <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-yellow-600 via-orange-600 to-pink-700"></span>
                 <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-amber-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
                 <span className="relative text-white">Submit</span>
-              </a>
+              </div>
             </button>
           </div>
         </form>
